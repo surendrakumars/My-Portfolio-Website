376b4e636610ad7c946fdc827cc0d37d68d4556d
Surendra Kumar S <surendrakumars7401@gmail.com>
2025-11-10 18:41:04 +0530

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+function scrollToHome(){
+    const homeSection = document.querySelector(".hero-section");
+    homeSection.scrollIntoView();
+}
+function scrollToAbout(){
+    const homeSection = document.querySelector(".about-section");
+    homeSection.scrollIntoView();
+}
+function scrollToServices(){
+    const homeSection = document.querySelector(".services-section");
+    homeSection.scrollIntoView();
+}
+function scrollToContact(){
+    const homeSection = document.querySelector(".contact-section");
+    homeSection.scrollIntoView();
+}
